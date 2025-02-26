a2622dc4763bf162aadf956a5b16c3c15aef27ec
Mark Feaver <mark.feaver@gmail.com>
2025-02-27 08:58:29 +1100

    Initial commit

---
+++ ShhArtTests/ShhArtTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShhArtTests.swift
+//  ShhArtTests
+//
+//  Created by Mark Feaver on 27/2/2025.
+//
+
+import Testing
+@testable import ShhArt
+
+struct ShhArtTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ShhArtUITests/ShhArtUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ShhArtUITestsLaunchTests.swift
+//  ShhArtUITests
+//
+//  Created by Mark Feaver on 27/2/2025.
+//
+
+import XCTest
+
+final class ShhArtUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
